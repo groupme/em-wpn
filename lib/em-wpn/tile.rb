0a0c59925381a92487f356bd56bd7be824faff4d
Andy Cheung <andy@groupme.com>
2012-02-15 11:02:54 -0500

    Update tile elements
    Add support for back-of-tile elements, such as title, content, and background
    image. Also add support for Id attribute on the tile to address secondary
    tiles.

--- lib/em-wpn/tile.rb
+++ lib/em-wpn/tile.rb
@@ -13,29 +13,48 @@ module EventMachine
 
       # <?xml version="1.0" encoding="utf-8"?>
       # <wp:Notification xmlns:wp="WPNotification">
-      # <wp:Tile>
-      # <wp:BackgroundImage>/image.png</wp:BackgroundImage>
-      # <wp:Count>999</wp:Count>
-      # <wp:Title>Title</wp:Title>
-      # </wp:Tile>
+      #   <wp:Tile>
+      #     <wp:Count>999</wp:Count>
+      #     <wp:Title>Title</wp:Title>
+      #     <wp:BackgroundImage>/image.png</wp:BackgroundImage>
+      #
+      #     <wp:BackTitle>Dave</wp:BackTitle>
+      #     <wp:BackContent>I'm on the back of the tile</wp:BackContent>
+      #     <wp:BackBackgroundImage>/background_image.png</wp:BackBackgroundImage>
+      #   </wp:Tile>
       # </wp:Notification>
+      #
+      # If any of the content on the back of the tile is left blank, then those
+      # elements are generated with the Action => "Clear" attribute, which should
+      # clear them on the tile.
       def generate_body
         builder = Nokogiri::XML::Builder.new(:encoding => "utf-8")
 
         builder.Notification("xmlns:wp" => "WPNotification") do |notification|
           builder.parent.namespace = builder.parent.namespace_definitions.last
 
-          notification.Tile do |tile|
-            if properties[:background_image]
-              tile.BackgroundImage properties[:background_image]
-            end
+          tile_attributes = properties[:id] ? {"Id" => properties[:id]} : {}
 
+          notification.Tile(tile_attributes) do |tile|
             tile.Count properties[:count] if properties[:count]
             tile.Title properties[:title] if properties[:title]
+            tile.BackgroundImage properties[:background_image] if properties[:background_image]
+
+            tile.BackTitle clear_empty_element(properties[:back_title])
+            tile.BackContent clear_empty_element(properties[:back_content])
+            tile.BackBackgroundImage clear_empty_element(properties[:back_background_image])
           end
         end
 
-        builder.to_xml
+        builder.to_xml(:save_with => Nokogiri::XML::Node::SaveOptions::NO_EMPTY_TAGS | Nokogiri::XML::Node::SaveOptions::FORMAT)
+      end
+
+      def clear_empty_element(content)
+        if content && content.to_s.size > 0
+          content
+        else
+          {"Action" => "Clear"}
+        end
       end
     end
   end
--- spec/em-wpn/tile_spec.rb
+++ spec/em-wpn/tile_spec.rb
@@ -2,11 +2,15 @@ require "spec_helper"
 
 describe EventMachine::WPN::Tile do
   describe "#body" do
-    it "builds body for title, count" do
+    it "builds body for :title, :count, :back_content, :back_title, :background_image, and :back_background_image" do
       notification = EM::WPN::Tile.new("http://example.com",
         {
           :count => 5,
-          :title => "Hello"
+          :title => "Hello",
+          :back_title => "Dave",
+          :back_content => "I'm on the back",
+          :background_image => "http://example.com/image.png",
+          :back_background_image => "http://example.com/back_image.png"
         }
       )
       notification.body.should == <<-XML
@@ -15,28 +19,78 @@ describe EventMachine::WPN::Tile do
   <wp:Tile>
     <wp:Count>5</wp:Count>
     <wp:Title>Hello</wp:Title>
+    <wp:BackgroundImage>http://example.com/image.png</wp:BackgroundImage>
+    <wp:BackTitle>Dave</wp:BackTitle>
+    <wp:BackContent>I'm on the back</wp:BackContent>
+    <wp:BackBackgroundImage>http://example.com/back_image.png</wp:BackBackgroundImage>
   </wp:Tile>
 </wp:Notification>
 XML
     end
 
-
-    it "builds body for background image" do
+    it "sets an id on the tile if it's passed in" do
       notification = EM::WPN::Tile.new("http://example.com",
         {
-          :background_image => "/image.png",
-          :title => "Hello"
+          :id => 1,
+          :title => "Hi"
+        }
+      )
+      notification.body.should == <<-XML
+<?xml version="1.0" encoding="utf-8"?>
+<wp:Notification xmlns:wp="WPNotification">
+  <wp:Tile Id="1">
+    <wp:Title>Hi</wp:Title>
+    <wp:BackTitle Action="Clear"></wp:BackTitle>
+    <wp:BackContent Action="Clear"></wp:BackContent>
+    <wp:BackBackgroundImage Action="Clear"></wp:BackBackgroundImage>
+  </wp:Tile>
+</wp:Notification>
+XML
+    end
+
+    it "sets the 'Action' = 'Clear' attribute on any elements on the back of the tile if they are nil" do
+      notification = EM::WPN::Tile.new("http://example.com",
+        {
+          :title => "Hi",
+          :back_title => nil,
+          :back_content => nil,
+          :back_background_image => nil
         }
       )
       notification.body.should == <<-XML
 <?xml version="1.0" encoding="utf-8"?>
 <wp:Notification xmlns:wp="WPNotification">
   <wp:Tile>
-    <wp:BackgroundImage>/image.png</wp:BackgroundImage>
-    <wp:Title>Hello</wp:Title>
+    <wp:Title>Hi</wp:Title>
+    <wp:BackTitle Action="Clear"></wp:BackTitle>
+    <wp:BackContent Action="Clear"></wp:BackContent>
+    <wp:BackBackgroundImage Action="Clear"></wp:BackBackgroundImage>
   </wp:Tile>
 </wp:Notification>
 XML
+    end
+
+    it "also sets 'Action' => 'Clear' on back elements of the tile if they are blank" do
+      notification = EM::WPN::Tile.new("http://example.com",
+        {
+          :title => "Hi",
+          :back_title => "",
+          :back_content => "",
+          :back_background_image => ""
+        }
+      )
+      notification.body.should == <<-XML
+<?xml version="1.0" encoding="utf-8"?>
+<wp:Notification xmlns:wp="WPNotification">
+  <wp:Tile>
+    <wp:Title>Hi</wp:Title>
+    <wp:BackTitle Action="Clear"></wp:BackTitle>
+    <wp:BackContent Action="Clear"></wp:BackContent>
+    <wp:BackBackgroundImage Action="Clear"></wp:BackBackgroundImage>
+  </wp:Tile>
+</wp:Notification>
+      XML
+
     end
 
     it "accepts string keys" do
@@ -52,6 +106,9 @@ XML
   <wp:Tile>
     <wp:Count>5</wp:Count>
     <wp:Title>Hello</wp:Title>
+    <wp:BackTitle Action="Clear"></wp:BackTitle>
+    <wp:BackContent Action="Clear"></wp:BackContent>
+    <wp:BackBackgroundImage Action="Clear"></wp:BackBackgroundImage>
   </wp:Tile>
 </wp:Notification>
 XML
@@ -69,6 +126,9 @@ XML
 <wp:Notification xmlns:wp="WPNotification">
   <wp:Tile>
     <wp:Title>Mike &amp; Ike &gt; Now &amp; Later</wp:Title>
+    <wp:BackTitle Action="Clear"></wp:BackTitle>
+    <wp:BackContent Action="Clear"></wp:BackContent>
+    <wp:BackBackgroundImage Action="Clear"></wp:BackBackgroundImage>
   </wp:Tile>
 </wp:Notification>
 XML
